9031a7052359af4ae1fe2ef06716bace30139253
Benjamin Hollway <benjamin@nothingrandom.com>
2020-03-06 10:30:57 +0100

    Tests for all date periods

--- tests/index.js
+++ tests/index.js
@@ -7,6 +7,24 @@ test('1 day', (t) => {
   t.is(ydmoDate, jsDate);
 });
 
+test('5 weeks', (t) => {
+  const ydmoDate = ydmo('5w').getTime();
+  const jsDate = new Date().getTime() + ((60 * 60 * 24 * 7) * 5 * 1000);
+  t.is(ydmoDate, jsDate);
+});
+
+test('3 months', (t) => {
+  const ydmoDate = ydmo('3mo').getTime();
+  const jsDate = new Date().getTime() + (((60 * 60 * 24 * 365) / 12) * 3 * 1000);
+  t.is(ydmoDate, jsDate);
+});
+
+test('2 years', (t) => {
+  const ydmoDate = ydmo('2y').getTime();
+  const jsDate = new Date().getTime() + ((60 * 60 * 24 * 365) * 2 * 1000);
+  t.is(ydmoDate, jsDate);
+});
+
 test('1 ago day', (t) => {
   const ydmoDate = ydmo('-1d').getTime();
   const jsDate = new Date().getTime() - ((60 * 60 * 24) * 1000);
